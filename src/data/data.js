92ffa5140c09bd8a3c36804a6b04e120f47f77ab
Ariana Chuquiruna <ari.chuquiruna@gmail.com>
2021-07-29 16:35:21 -0300

    adding project information

--- src/data/data.js
+++ src/data/data.js
@@ -1,72 +1,70 @@
 export const skillsNames = ['HTML', 'CSS', 'Javascript', 'Figma', 'Sass', 'Bootstrap', 'React', 'Redux', 'GIT', 'Firebase', 'NodeJS', 'ExpressJS', 'MongoDB', 'Socket.io', ''];
 export const skillsLogos = ['fa-html5', 'fa-css3-alt', 'fa-js-square', 'fa-figma', 'fa-sass', 'fa-bootstrap', 'fa-react', 'redux.png', 'fa-git-alt', 'firebase.png', 'fa-node-js', 'express.png', 'mongodb.png', 'socket.png', ''];
 
-// export const imagesProyects = [ 'mundoviole', 'portfolio', 'lamilonga', 'calendarapp', 'journalapp', 'energy',  ]
-// export const imagesProyects = [ 'mundoviole', 'mundoviole', 'mundoviole', 'mundoviole', 'mundoviole' ];
 export const imagesProyects = [ 
     {
         logo: 'mundoviole.png',
         img: 'mundo-viole.png',
         title: 'El Mundo de Viole',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut mauris non felis ornare bibendum vel ut lorem. Suspendisse ultrices feugiat pellentesque.  Vestibulum ut mauris non felis ornare bibendum vel ut lorem.',
+        description: 'Sitio web creado para una niña donde se muestran sus cuentos y dibujos, con un diseño intuitivo para que la administradora gestione estos.',
         frontendTech: [ 'fa-html5', 'fa-sass', 'fa-react', 'redux.png'],
         backendTech: [ 'fa-node-js', 'express.png', 'mongodb.png' ],
-        frontendGit: 'asd',
-        BackendGit: 'asd',
-        liveLink: 'asd'
+        frontendGit: 'https://github.com/ariachuqui/el-mundo-de-viole',
+        BackendGit: 'https://github.com/ariachuqui/el-mundo-de-viole-backend',
+        liveLink: 'https://mundo-viole.netlify.app/'
     },
     {
         logo: 'portfolio.png',
         img: 'portfolio.png',
         title: 'Portfolio',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut mauris non felis ornare bibendum vel ut lorem. Suspendisse ultrices feugiat pellentesque.  Vestibulum ut mauris non felis ornare bibendum vel ut lorem.',
+        description: 'Portfolio personal para demostrar mis habilidades y experiencias. Las animaciones en los titulos y el efecto 3D en “Skills” son creados por mí.',
         frontendTech: [ 'fa-html5', 'fa-sass', 'fa-react'],
         backendTech: null,
-        frontendGit: 'asd',
+        frontendGit: 'https://github.com/ariachuqui/portfolio',
         BackendGit: null,
-        liveLink: 'asd'
+        liveLink: 'https://portfolio-ariachuqui.netlify.app/'
     },
     {
-        logo: 'mundoviole.png',
+        logo: 'la-milonga.png',
         img: 'la-milonga.png',
         title: 'La Milonga Argentina',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut mauris non felis ornare bibendum vel ut lorem. Suspendisse ultrices feugiat pellentesque.  Vestibulum ut mauris non felis ornare bibendum vel ut lorem.',
+        description: 'Sitio web freelance para la Milonga Argentina, creado en equipo. Diseñamos un blog con un sistema muy intuitivo en el que el cliente puede gestionar la sección de artículos y albums por su propia cuenta.',
         frontendTech: [ 'fa-html5', 'fa-sass', 'fa-react', 'redux.png'],
         backendTech: [ 'firebase.png' ],
         frontendGit: null,
         BackendGit: null,
-        liveLink: 'asd'
+        liveLink: 'https://www.lamilongaargentina.com.ar/'
     },
     {
-        logo: 'mundoviole.png',
+        logo: 'calendarapp.png',
         img: 'calendarapp.png',
         title: 'Calendar App',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut mauris non felis ornare bibendum vel ut lorem. Suspendisse ultrices feugiat pellentesque.  Vestibulum ut mauris non felis ornare bibendum vel ut lorem.',
+        description: 'Aplicación web con autenticación y registro de usuarios, donde se pueden agendar eventos en un calendario general, podrás ver los eventos creador por otros y los demás pordrán ver los tuyos.',
         frontendTech: [ 'fa-html5', 'fa-sass', 'fa-react', 'redux.png'],
         backendTech: [ 'fa-node-js', 'express.png', 'mongodb.png' ],
-        frontendGit: 'asd',
-        BackendGit: 'asd',
+        frontendGit: 'https://github.com/ariachuqui/calendar-app-frontend',
+        BackendGit: 'https://github.com/ariachuqui/calendar-app-backend',
         liveLink: 'https://ariachuqui.github.io/calendar-app-frontend/'
     },
     {
-        logo: 'mundoviole.png',
+        logo: 'journal.png',
         img: 'journal.png',
         title: 'Journal App',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut mauris non felis ornare bibendum vel ut lorem. Suspendisse ultrices feugiat pellentesque.  Vestibulum ut mauris non felis ornare bibendum vel ut lorem.',
+        description: 'Aplicación web con autenticación y registro de usuarios, donde podrás crear notas y llevarlas a todos lados.',
         frontendTech: [ 'fa-html5', 'fa-sass', 'fa-react', 'redux.png'],
         backendTech: [ 'firebase.png' ],
-        frontendGit: 'asd',
-        BackendGit: 'asd',
+        frontendGit: 'https://github.com/ariachuqui/journal-app',
+        BackendGit: null,
         liveLink: 'https://ariachuqui.github.io/journal-app/'
     },
     {
-        logo: 'mundoviole.png',
+        logo: 'energy.png',
         img: 'energy.png',
         title: 'Energy',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut mauris non felis ornare bibendum vel ut lorem. Suspendisse ultrices feugiat pellentesque.  Vestibulum ut mauris non felis ornare bibendum vel ut lorem.',
+        description: 'Sitio Web de clases deportivas online. Los fondos y las animaciones son creadas por mí, se pueden visibilizar mís habilidades en el diseño y creatividad a la hora de crear un sitio web.',
         frontendTech: [ 'fa-html5', 'fa-css3-alt', 'fa-js-square' ],
         backendTech: null,
-        frontendGit: 'asd',
+        frontendGit: 'https://github.com/ariachuqui/energy-frontend',
         BackendGit: null,
         liveLink: 'https://energy-codo-a-codo.netlify.app/'
     },
